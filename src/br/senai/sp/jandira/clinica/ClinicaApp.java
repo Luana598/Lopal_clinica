6ed798a64d287f6ca4588838c815ae91f5150b59
LUANA MARIANA LOPES BOMFIM <25132776@CFP127ALUNO.LOCAL>
2025-02-10 11:25:18 -0300

    criação dos atributos e assinaturas dos métodos

--- src/br/senai/sp/jandira/clinica/ClinicaApp.java
+++ src/br/senai/sp/jandira/clinica/ClinicaApp.java
@@ -1,5 +1,34 @@
 package br.senai.sp.jandira.clinica;
 
-public class ClinicaApp {
+import java.time.LocalDate;
+
+public class Paciente {
+
+    String nome;
+    double peso;
+    double altura;
+    String genero;
+    String telefone;
+    LocalDate dataNascimento;
+
 }
 
+public void marcarConsulta(){
+
+}
+
+public void calcularIdade(){
+
+}
+
+public void calcularImc(){
+
+}
+
+public void classificarImc(){
+
+}
+
+public void exibirDados(){
+
+}
